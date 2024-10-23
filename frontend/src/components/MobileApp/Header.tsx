58067f97cc7452cbaabc7caffbc4f01cc41841ea
João <joao@roboflow.com>
2024-10-23 13:45:15 -0500

    put links in headers

--- frontend/src/components/MobileApp/Header.tsx
+++ frontend/src/components/MobileApp/Header.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header: React.FC<{ step: "picture" | "prompt" }> = ({ step }) => {
   return (
     <div className="w-full justify-center flex flex-col items-center gap-4 mb-4">
-      <div className="w-2/5 py-2">
+      <Link className="w-2/5 py-2" to="https://roboflow.com" target="_blank">
         <img src="public/roboflow-logo.png" alt="Roboflow" />
-      </div>
+      </Link>
       <div className="flex flex-col gap-4 w-full">
         <h1 className="text-2xl font-semibold">
           {step === "picture" ? "1. Add an image" : "2. Add a prompt"}
