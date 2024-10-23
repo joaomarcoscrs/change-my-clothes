71c29ea9a16d0787fa53a22ce6884d1519cd937e
João <joao@roboflow.com>
2024-10-23 09:17:16 -0500

    prompt input styling

--- frontend/src/components/PromptInput.tsx
+++ frontend/src/components/PromptInput.tsx
@@ -14,7 +14,7 @@ const PromptInput: React.FC<PromptInputProps> = ({ prompt, setPrompt }) => {
         value={prompt}
         onChange={(e) => setPrompt(e.target.value)}
         placeholder='"Tropical clothes"'
-        className="w-full h-12 p-2 font-mono text-sm font-light border text-gray-300 border-gray-400 bg-white/10 rounded-lg focus:outline-none focus:ring-1 focus:ring-purple-700"
+        className="w-full h-12 p-2 font-mono text-sm font-light border text-gray-300 border-gray-400 bg-white/5 rounded-lg focus:outline-none focus:ring-1 focus:ring-purple-700 hover:bg-white/10 focus:bg-white/10 hover:border-purple-400 focus:border-purple-400"
       />
     </div>
   );
